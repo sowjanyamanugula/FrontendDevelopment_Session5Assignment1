e389a91e76010c96b5277dbc7ff25db4955fafb0
sowjanyamanugula <manugulasowjanya@gmail.com>
2017-01-10 11:57:18 +0530

    Changes

---
+++ js/app.js
@@ -0,0 +1,5 @@
+ var name="sonia";
+        var age="25";
+        var dateofbirth="6th dec";
+        var placeofbirth="Hyderabad";
+        console.log("Name="+name+","+"Age="+age+","+"Date of Birth="+dateofbirth+","+"Place of Birth="+placeofbirth);
